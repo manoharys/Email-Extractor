fb3ad0eb504fac48c4d6e752f8723d0960263360
manohar <manumanohar1527@gmail.com>
2020-04-27 22:37:07 +0530

    output only emails

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const TextAreaOne = document.querySelector('textarea[name="inputText"');
 const TextAreaTwo = document.querySelector('textarea[name="outputText"');
 const button = document.querySelector('button');
-
+const counter = document.querySelector('.counter');
 //Adding event listener to button 
 button.addEventListener('click', getEmails);
 
@@ -9,6 +9,6 @@ function getEmails() {
     let temp = TextAreaOne.value;
     let expression = /([A-Za-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi; //regular expression for extracting emails
     let mails = temp.match(expression);
-
-    console.log(mails);
+    counter.innerText = mails.length + " emails found";
+    TextAreaTwo.innerText = mails; //outputting emails
 }
